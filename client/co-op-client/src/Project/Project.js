929c44602c720f52007ff53be3f37222230a11bc
nika <nsaka16@freeuni.edu.ge>
2021-03-10 00:01:32 +0400

    update

--- client/co-op-client/src/Project/Project.js
+++ client/co-op-client/src/Project/Project.js
@@ -1,7 +1,13 @@
+import './Project.css'
+
 function Project(props){
     return(
         <div className='project'>
-
+            <h1>Name: {props.name}</h1>
+            <h1>Link: {props.link}</h1>
+            <h1>Description: {props.description}</h1>
         </div>
     );
 }
+
+export default Project;
--- client/co-op-client/src/Project/ProjectProxy.js
+++ client/co-op-client/src/Project/ProjectProxy.js
@@ -1,4 +1,4 @@
-import './Project.css'
+import './ProjectProxy.css'
 
 function ProjectProxy(props){
     return(
